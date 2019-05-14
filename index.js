43ae314495cf92a0787cfc1476724114433ad829
lhaaits <l.haaitsma@gmail.com>
2019-05-14 17:09:38 +0200

    added login by email, changed api calls

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+require("dotenv").config();
 
-const userId = process.env.TGTG_USER_ID;
-const userToken = process.env.TGTG_USER_TOKEN;
-const gmApiKey = process.env.GM_API_KEY;
+console.log(process.env.EMAIL);
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+const latitude = process.env.LAT;
+const longitude = process.env.LONG;
 
 const main = async () => {
-  const geocodeResponse = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${process.argv[2]}&key=${gmApiKey}`);
-  if (geocodeResponse.status > 300) throw new Error('could not get geocode response');
-  const geocode = await geocodeResponse.json();
+  const TGTG_URI = "https://apptoogoodtogo.com/index.php/api_tgtg";
+ 
+  const response = await fetch(
+    `${TGTG_URI}/login?email=${email}&password=${password}`,
+    {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "none",
+        "accept-language": "nl-NL;q=1.0, nl-NL;q=0.9"
+      }
+    }
+  );
+  const login = await response.json();
+ 
+  const access_token = login.access_token;
+  const user_id = login.user_id;
+  const user_token = login.user_token;
 
-  if (geocode.results.length < 1) throw new Error('could not get geocode response');
+  const responseList = await fetch(
+    `${TGTG_URI}/list_all_business_map_v5_gz?access_token=${access_token}&user_id=${user_id}&user_token=${user_token}&latitude=${latitude}&longitude=${longitude}`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "content-type": "none",
+        "accept-language": "nl-NL;q=1.0, nl-NL;q=0.9"
+      }
+    }
+  );
+  const allBusinesses = await responseList.json();
 
-  const geocodeResults = geocode.results[0].geometry.location;
+  // A bit hacky way to find local businesses
+  const local = allBusinesses.info.filter(i => Math.abs(parseFloat(i.latitude)-latitude) < 0.01 && Math.abs(parseFloat(i.longitude) - longitude) < 0.01);
 
-  const bearerToken = Buffer.from(`ACCESS:${userId}:${userToken}`).toString('base64');
-  console.log('bearerToken:', bearerToken);
-  const response = await fetch(`https://apptoogoodtogo.com/index.php/api_tgtg/list_businessv5`, {
-    method: 'POST',
-    headers: {
-      'accept': 'application/json',
-      'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
-      'accept-language': 'de-DE;q=1.0, en-DE;q=0.9',
-      'user-agent': 'TooGoodToGo/18.12.1 (637) (iPhone/Unknown; iOS 12.1; Scale/3.00)',
-      'authorization': `Bearer ${bearerToken}`,
-    },
-    body: `category=0&filter_end=24%3A00%3A00&filter_start=00%3A00%3A00&filter_type=1&isOnline=0&is_favorite=0&latitude=${geocodeResults.lat}&limit=20&localtime=14%3A20%3A06&longitude=${geocodeResults.lng}&page_no=1&user_id=${userId}&user_token=${userToken}`,
-    compress: true,
-  });
-
-  const json = await response.json();
-
-  console.log('json', JSON.stringify(json, undefined, 2));
-
-  for(const business of json.info) {
-    console.log('-', business['business_name']);
-  }
-}
+  const business_id = 123456
+  // TODO this gives a message that I should update the app, so I think this is an old version of the API endpoint
+  const responseList2 = await fetch(
+    `${TGTG_URI}/get_businessv5?user_id=${user_id}&localtime=utc&created_by=${business_id}`,
+    {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "none",
+        "accept-language": "nl-NL;q=1.0, nl-NL;q=0.9"
+      }
+    }
+  );
+  const business = await responseList2.json();
+};
 
 main();
